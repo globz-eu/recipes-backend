800625293c8b9933a054d001f0a566aad9408d51
Stefan Dieterle <stefand@artcom.de>
2019-10-18 00:00:25 +0200

    Add frontend host to CORS origin white list

--- recipes_backend/settings.py
+++ recipes_backend/settings.py
@@ -25,7 +25,8 @@ ALLOWED_HOSTS = [setting('ALLOWED_HOST')]
 
 CORS_ORIGIN_WHITELIST = [
     'http://127.0.0.1:8080',
-    'http://localhost:8080'
+    'http://localhost:8080',
+    'http://{}'.format(setting('FRONTEND_HOST'))
 ]
 
 # Application definition
