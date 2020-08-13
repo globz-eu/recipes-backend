0aba94d936e24c22db5b71cdcc1a56ffff2748f4
Stefan Dieterle <stefand@artcom.de>
2020-08-13 11:24:06 +0200

    Refactor

--- recipes/tests/models/test_recipe.py
+++ recipes/tests/models/test_recipe.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from ..models import Recipe, Ingredient
+from recipes.models import Recipe, Ingredient
 
 
 class RecipeTest(TestCase):
@@ -19,18 +19,3 @@ class RecipeTest(TestCase):
             [ingredient.name for ingredient in lekker.ingredient_set.all()],
             ['aubergine', 'garlic']
         )
-
-
-class IngredientTest(TestCase):
-
-    def setUp(self):
-        lekker = Recipe.objects.create(name='Lekker', servings=3, instructions='Stir well')
-        garlic = Ingredient.objects.create(name='garlic', plural='garlic')
-        aubergine = Ingredient.objects.create(name='aubergine', plural='aubergines')
-        aubergine.recipes.add(lekker)
-        garlic.recipes.add(lekker)
-
-    def test_db_fields(self):
-        aubergine = Ingredient.objects.get(name='aubergine')
-        self.assertEqual(aubergine.plural, 'aubergines')
-        self.assertEqual(aubergine.recipes.first().name, 'Lekker')
