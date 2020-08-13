1d3d621c66fe88b5565b9d2a70266f10acaeeea4
Stefan Dieterle <stefand@artcom.de>
2020-08-13 11:04:49 +0200

    Add ingredient model

--- recipes/models.py
+++ recipes/models.py
@@ -6,3 +6,15 @@ class Recipe(models.Model):
     name = models.CharField(max_length=100)
     servings = models.PositiveSmallIntegerField(default=4)
     instructions = models.TextField(max_length=1000)
+
+    class Meta:
+        ordering = ['name']
+
+
+class Ingredient(models.Model):
+    recipes = models.ManyToManyField(Recipe)
+    name = models.CharField(max_length=100)
+    plural = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['name']
--- recipes/tests/test_models.py
+++ recipes/tests/test_models.py
@@ -1,13 +1,36 @@
 from django.test import TestCase
-from ..models import Recipe
+from ..models import Recipe, Ingredient
 
 
 class RecipeTest(TestCase):
 
     def setUp(self):
-        Recipe.objects.create(name='Lekker', servings=3, instructions='Stir well')
+        lekker = Recipe.objects.create(name='Lekker', servings=3, instructions='Stir well')
+        garlic = Ingredient.objects.create(name='garlic', plural='garlic')
+        aubergine = Ingredient.objects.create(name='aubergine', plural='aubergines')
+        aubergine.recipes.add(lekker)
+        garlic.recipes.add(lekker)
 
     def test_db_fields(self):
-        recipe_lekker = Recipe.objects.get(name='Lekker')
-        self.assertEqual(recipe_lekker.instructions, 'Stir well')
-        self.assertEqual(recipe_lekker.servings, 3)
+        lekker = Recipe.objects.get(name='Lekker')
+        self.assertEqual(lekker.instructions, 'Stir well')
+        self.assertEqual(lekker.servings, 3)
+        self.assertEqual(
+            [ingredient.name for ingredient in lekker.ingredient_set.all()],
+            ['aubergine', 'garlic']
+        )
+
+
+class IngredientTest(TestCase):
+
+    def setUp(self):
+        lekker = Recipe.objects.create(name='Lekker', servings=3, instructions='Stir well')
+        garlic = Ingredient.objects.create(name='garlic', plural='garlic')
+        aubergine = Ingredient.objects.create(name='aubergine', plural='aubergines')
+        aubergine.recipes.add(lekker)
+        garlic.recipes.add(lekker)
+
+    def test_db_fields(self):
+        aubergine = Ingredient.objects.get(name='aubergine')
+        self.assertEqual(aubergine.plural, 'aubergines')
+        self.assertEqual(aubergine.recipes.first().name, 'Lekker')
