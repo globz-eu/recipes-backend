ed3ed268b3739feda29681e4c898a12ba07989cb
Stefan Dieterle <stefand@artcom.de>
2020-08-15 22:14:44 +0200

    Add RecipeManager mode manager

--- recipes/models.py
+++ recipes/models.py
@@ -10,12 +10,38 @@ class Ingredient(models.Model):
         constraints = [models.UniqueConstraint(fields=['name'], name='unique_ingredient_name')]
 
 
+class RecipeManager(models.Manager):  # pylint: disable = too-few-public-methods
+    def create(self, recipe, ingredient_amounts):
+        recipe = self.model(
+            name=recipe['name'],
+            servings=recipe['servings'],
+            instructions=recipe['instructions']
+        )
+        recipe.save()
+        for ingredient_amount in ingredient_amounts:
+            ingredient, _ = Ingredient.objects.get_or_create(
+                name=ingredient_amount['ingredient']['name'],
+                plural=ingredient_amount['ingredient']['plural']
+            )
+            unit, _ = Unit.objects.get_or_create(name=ingredient_amount['unit']['name'])
+            recipe.ingredient_amounts.add(
+                ingredient,
+                through_defaults={
+                    'unit': unit,
+                    'quantity': ingredient_amount['quantity']
+                }
+            )
+        return recipe
+
+
 class Recipe(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=100)
     servings = models.PositiveSmallIntegerField(default=4)
     instructions = models.TextField(max_length=1000)
     ingredient_amounts = models.ManyToManyField(Ingredient, through='IngredientAmount')
+    objects = models.Manager()
+    recipes = RecipeManager()
 
     class Meta:
         ordering = ['name']
