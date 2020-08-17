0b6d9a05aabae9c68375779c8321f4c4bfd61643
Stefan Dieterle <stefand@artcom.de>
2020-08-17 14:11:11 +0200

    Add get method to RecipeManager

--- recipes/tests/helpers/test_helpers.py
+++ recipes/tests/helpers/test_helpers.py
@@ -3,4 +3,4 @@ from django.test import TestCase
 
 class GetIngredientAmountsTest(TestCase):
     def test_function_returns_expected_queryset(self):
-        self.fail()
+        self.fail('not implemented')
