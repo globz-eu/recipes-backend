e8c3af49e90f5958af843ee0c33413404b6130b7
Stefan Dieterle <stefand@artcom.de>
2020-08-19 23:11:21 +0200

    Use test data

--- recipes/tests/views/__init__.py
+++ recipes/tests/views/__init__.py
@@ -1,4 +1,4 @@
 from rest_framework import status
 from django.urls import reverse
-from recipes.tests.views.setup import InitializeRecipes, Authenticate, SetPayloads
+from recipes.tests.views.setup import InitializeRecipes, Authenticate
 from recipes.models import Recipe
--- recipes/tests/views/recipe_detail/test_update.py
+++ recipes/tests/views/recipe_detail/test_update.py
@@ -1,20 +1,21 @@
 import json
-from .. import status, reverse, InitializeRecipes, Authenticate, SetPayloads, Recipe
+from recipes.tests.helpers import get_recipe_data
+from recipes.tests.views import status, reverse, InitializeRecipes, Authenticate, Recipe
 from . import RecipeModelSerializer
 
 
-class UpdateSingleRecipeTest(InitializeRecipes, Authenticate, SetPayloads):
+class UpdateSingleRecipeTest(InitializeRecipes, Authenticate):
     """ Test updating an existing recipe """
 
     def setUp(self):
         InitializeRecipes.setUp(self)
         Authenticate.setUp(self)
-        SetPayloads.setUp(self)
+        self.recipe_data = get_recipe_data('lekker')
 
     def test_valid_update_recipe(self):
         response = self.client.put(
             reverse('recipe_detail', kwargs={'pk': self.lekker.pk}),
-            data=json.dumps(self.valid_payload),
+            data=json.dumps(self.recipe_data['recipe']),
             content_type='application/json'
         )
         recipe = Recipe.objects.get(pk=self.lekker.pk)
@@ -25,22 +26,22 @@ class UpdateSingleRecipeTest(InitializeRecipes, Authenticate, SetPayloads):
     def test_invalid_update_recipe(self):
         response = self.client.put(
             reverse('recipe_detail', kwargs={'pk': self.pas_mal.pk}),
-            data=json.dumps(self.invalid_payload),
+            data=json.dumps({}),
             content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
-class UpdateSingleRecipeUnauthenticatedTest(InitializeRecipes, SetPayloads):
+class UpdateSingleRecipeUnauthenticatedTest(InitializeRecipes):
     """ Test updating an existing recipe without authentication """
 
     def setUp(self):
         InitializeRecipes.setUp(self)
-        SetPayloads.setUp(self)
+        self.recipe_data = get_recipe_data('lekker')
 
     def test_valid_update_recipe(self):
         response = self.client.put(
             reverse('recipe_detail', kwargs={'pk': self.lekker.pk}),
-            data=json.dumps(self.valid_payload),
+            data=json.dumps(self.recipe_data['recipe']),
             content_type='application/json'
         )
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
@@ -48,6 +49,6 @@ class UpdateSingleRecipeUnauthenticatedTest(InitializeRecipes, SetPayloads):
     def test_invalid_update_recipe(self):
         response = self.client.put(
             reverse('recipe_detail', kwargs={'pk': self.pas_mal.pk}),
-            data=json.dumps(self.invalid_payload),
+            data=json.dumps({}),
             content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
