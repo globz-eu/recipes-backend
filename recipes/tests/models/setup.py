62d34ac2ea564ffa96abee54ab58d544c188e2a6
Stefan Dieterle <stefand@artcom.de>
2020-08-19 22:20:13 +0200

    Use model manager

--- recipes/tests/models/setup.py
+++ recipes/tests/models/setup.py
@@ -1,19 +1,10 @@
 from django.test import TestCase
-from recipes.models import Recipe, Ingredient, Unit
+from recipes.models import Recipe
+from recipes.tests.helpers import get_recipe_data
 
 
 class RecipeIngredients(TestCase):
 
     def setUp(self):
-        self.lekker = Recipe.objects.create(name='Lekker', servings=3, instructions='Stir well')
-        self.garlic = Ingredient.objects.create(name='garlic', plural='garlic')
-        self.aubergine = Ingredient.objects.create(name='aubergine', plural='aubergines')
-        piece = Unit.objects.create(name='piece')
-        self.lekker.ingredient_amounts.add(
-            self.aubergine,
-            through_defaults={'unit': piece, 'quantity': 2}
-        )
-        self.lekker.ingredient_amounts.add(
-            self.garlic,
-            through_defaults={'unit': piece, 'quantity': 1}
-        )
+        recipe_data = get_recipe_data('lekker')
+        self.lekker = Recipe.recipes.create(**recipe_data)
